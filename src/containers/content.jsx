10ce5e46e44ff103e4131acb6dbef18bf3cf638d
LucasSovre <lucas.sovre@proton.me>
2023-05-30 12:42:03 +0200

    add content when loged-in

--- src/containers/content.jsx
+++ src/containers/content.jsx
@@ -11,6 +11,17 @@ export default function Content(){
 
     const {t} = useTranslation()
     const [percent, setPercent] = useState(10);
+    const [connected, setConnected] = useState(false);
+
+    //TODO for the web3 dev theses two states are the current investment and the current NRT holded by the user
+    const [currentInvestment, setCurrentInvestment] = useState(0);
+    const [currentNRT, setCurrentNRT] = useState(0);
+
+    //TODO for the web3 dev implement wallet connection here
+    function login(){
+        //this function is fired when connect wallet button is clicked
+        setConnected(true)
+    }
 
     useEffect(()=>{
         const now = new Date();
@@ -59,10 +70,35 @@ export default function Content(){
                     </div>
                 </div>
             </Section>
-            <Section title={t("whiteList")}>
-                <button className="main-btn" style={{width : '100%', justifyContent : "center"}}>
+            <Section title={t("WhiteList")}>
+                {connected ? 
+                <div className="info">
+                    <span>
+                        <p>{t("witheList_content_1")}</p><p><strong>1</strong></p> 
+                    </span>
+                    <span>
+                        <p>{t("witheList_content_2")}</p><p><strong>4</strong></p> 
+                    </span>
+                    <span>
+                        <p>{t("witheList_content_3")}</p><p><strong>3,000 USDC</strong></p> 
+                    </span>
+                    <span className="split"></span>
+                    <span>
+                        <p>{t("witheList_content_4")}</p><p><strong>{currentInvestment} USDC</strong></p> 
+                    </span>
+                    <span>
+                        <p>{t("witheList_content_5")}</p><p><strong>{currentNRT}</strong></p> 
+                    </span>
+                    <button className="main-btn" style={{width : '100%', justifyContent : "center"}}>
+                        {t("buy")}
+                    </button>
+                </div> 
+                : 
+                <button className="main-btn" style={{width : '100%', justifyContent : "center"}}
+                 onClick={()=>{login()}}>
                     {t("witheList_conection")}
                 </button>
+                }
             </Section>
         </div>
     )
