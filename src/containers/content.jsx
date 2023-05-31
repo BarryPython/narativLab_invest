c76fc404d67c672bfbf64b52ca6bd7343b6a2744
LucasSovre <lucas.sovre@proton.me>
2023-05-31 12:50:04 +0200

    add modal

--- src/containers/content.jsx
+++ src/containers/content.jsx
@@ -6,12 +6,14 @@ import { ReactComponent as Lock } from "../assets/icons/lock.svg";
 import { ReactComponent as Chart } from "../assets/icons/chart.svg";
 import { ReactComponent as Coin } from "../assets/icons/coin.svg";
 import { useEffect, useState } from "react";
+import Modal from "./modal";
 
 export default function Content(){
 
     const {t} = useTranslation()
     const [percent, setPercent] = useState(10);
     const [connected, setConnected] = useState(false);
+    const [modalAcivate, setmodalAcivate] = useState(false);
 
     //TODO for the web3 dev theses two states are the current investment and the current NRT holded by the user
     const [currentInvestment, setCurrentInvestment] = useState(0);
@@ -32,74 +34,79 @@ export default function Content(){
     },[])
 
     return(
-        <div className="content">
-            <Section title={t("private_sale")} Icon={Lock}>
+        <>  
+            <Modal active={modalAcivate} setActive={setmodalAcivate}/>
+            <div className="content">
+                <Section title={t("private_sale")} Icon={Lock}>
+                    <p>
+                        {t("private_sale_content")}
+                    </p>
+                </Section>
+                <Section title={t("public_sale")} Icon={Chart}>
                 <p>
-                    {t("private_sale_content")}
-                </p>
-            </Section>
-            <Section title={t("public_sale")} Icon={Chart}>
-            <p>
-                    {t("public_sale_content")}
-                </p>
-            </Section>
-            <Section title={t("raise_utility")} Icon={Coin}>
-            <ul>
-                <li>{t("raise_utility_content_1")}</li>
-                <li>{t("raise_utility_content_2")}</li>
-                <li>{t("raise_utility_content_3")}</li>
-            </ul>
-            </Section>
-            <Section title={t("info")}>
-                <div className="info">
-                    <span>
-                        <p>{t("info_content_1")}</p><p><strong>$0,05</strong></p>
-                    </span>
-                    <span>
-                        <p>{t("info_content_2")}</p><p><strong>$0,10</strong></p>
-                    </span>
-                    <span>
-                        <p>{t("info_content_3")}</p><p><strong>$600,000</strong></p>
-                    </span>
-                    <span>
-                        <p>{t("info_content_4")}</p><p className="time"><strong>{t("info_before").replace("X",100-percent)}</strong></p>
-                    </span>
-                    <div id="status-bar">
-                        <span style={{width : `${percent}%`}}>
+                        {t("public_sale_content")}
+                    </p>
+                </Section>
+                <Section title={t("raise_utility")} Icon={Coin}>
+                <ul>
+                    <li>{t("raise_utility_content_1")}</li>
+                    <li>{t("raise_utility_content_2")}</li>
+                    <li>{t("raise_utility_content_3")}</li>
+                </ul>
+                </Section>
+                <Section title={t("info")}>
+                    <div className="info">
+                        <span>
+                            <p>{t("info_content_1")}</p><p><strong>$0,05</strong></p>
                         </span>
+                        <span>
+                            <p>{t("info_content_2")}</p><p><strong>$0,10</strong></p>
+                        </span>
+                        <span>
+                            <p>{t("info_content_3")}</p><p><strong>$600,000</strong></p>
+                        </span>
+                        <span>
+                            <p>{t("info_content_4")}</p><p className="time"><strong>{t("info_before").replace("X",100-percent)}</strong></p>
+                        </span>
+                        <div id="status-bar">
+                            <span style={{width : `${percent}%`}}>
+                            </span>
+                        </div>
                     </div>
-                </div>
-            </Section>
-            <Section title={t("WhiteList")}>
-                {connected ? 
-                <div className="info">
-                    <span>
-                        <p>{t("witheList_content_1")}</p><p><strong>1</strong></p> 
-                    </span>
-                    <span>
-                        <p>{t("witheList_content_2")}</p><p><strong>4</strong></p> 
-                    </span>
-                    <span>
-                        <p>{t("witheList_content_3")}</p><p><strong>3,000 USDC</strong></p> 
-                    </span>
-                    <span className="split"></span>
-                    <span>
-                        <p>{t("witheList_content_4")}</p><p><strong>{currentInvestment} USDC</strong></p> 
-                    </span>
-                    <span>
-                        <p>{t("witheList_content_5")}</p><p><strong>{currentNRT}</strong></p> 
-                    </span>
-                    <button className="main-btn" style={{width : '100%', justifyContent : "center"}}>
-                        {t("buy")}
+                </Section>
+                <Section title={t("WhiteList")}>
+                    {connected ? 
+                    <div className="info">
+                        <span>
+                            <p>{t("witheList_content_1")}</p><p><strong>1</strong></p> 
+                        </span>
+                        <span>
+                            <p>{t("witheList_content_2")}</p><p><strong>4</strong></p> 
+                        </span>
+                        <span>
+                            <p>{t("witheList_content_3")}</p><p><strong>3,000 USDC</strong></p> 
+                        </span>
+                        <span className="split"></span>
+                        <span>
+                            <p>{t("witheList_content_4")}</p><p><strong>{currentInvestment} USDC</strong></p> 
+                        </span>
+                        <span>
+                            <p>{t("witheList_content_5")}</p><p><strong>{currentNRT}</strong></p> 
+                        </span>
+                        <button className="main-btn" style={{width : '100%', justifyContent : "center"}}
+                            onClick={()=>{setmodalAcivate(true)}}
+                        >
+                            {t("buy")}
+                        </button>
+                    </div> 
+                    : 
+                    <button className="main-btn" style={{width : '100%', justifyContent : "center"}}
+                    onClick={()=>{login()}}>
+                        {t("witheList_conection")}
                     </button>
-                </div> 
-                : 
-                <button className="main-btn" style={{width : '100%', justifyContent : "center"}}
-                 onClick={()=>{login()}}>
-                    {t("witheList_conection")}
-                </button>
-                }
-            </Section>
-        </div>
+                    }
+                </Section>
+            </div>
+        </>
     )
 }
---
+++ src/containers/modal.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+//svg
+import { ReactComponent as Close } from "../assets/icons/close.svg";
+import usdc from "../assets/icons/usdc.png";
+
+import "../style/modal.scss"
+
+export default function Modal({active, setActive}){
+
+    const {t} = useTranslation()
+    const [usdcInvest, setUsdcInvest] = useState(0)
+
+    function usdcToNrt(usdc){
+        //TODO change this function to convert usdc to NRT with the correct rate
+        return usdc / 0.01
+    }
+
+    function buy(e){
+        e.preventDefault() //do not remove this line
+        //TODO bellow make the magic to buy NRT
+        console.log(usdcInvest)
+    }
+    
+    return(
+        active ?
+        <div className="modal-container" onClick={()=>{setActive(false)}}>
+            <section>
+                <div className="header">
+                    <h2>{t("invest")}</h2>
+                    <button onClick={()=>{setActive(false)}}>
+                        <Close/>
+                    </button>
+                </div>
+                <form onSubmit={(e)=>{buy(e)}}>
+                    <div>
+                        <label>{t("invest_deposit")}</label>
+                        <span>
+                            <img src={usdc} alt=""/>
+                            <input type="number" placeholder="0" onChange={(e)=>{setUsdcInvest(e.target.value)}}/>
+                        </span>
+                    </div>
+                    <div>
+                        <p>{t("invest_aquired")}</p>
+                        <p><strong>{usdcToNrt(usdcInvest)} NRT</strong></p>
+                    </div>
+                    <button className="main-btn" style={{width : '100%', justifyContent : "center", marginTop : "20px"}}>
+                        {t("buy")}
+                    </button>
+                </form>
+            </section>
+        </div>
+        : null
+    )
+}
